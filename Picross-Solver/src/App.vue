08b8142ed2e8cc80e074f2f7652d81c2683aa8c5
SelenaLa6 <selenala066@gmail.com>
2025-03-03 10:56:03 -0500

    general format

--- Picross-Solver/src/App.vue
+++ Picross-Solver/src/App.vue
@@ -3,14 +3,29 @@
 
 <template>
   <head>
-    <link href="/src/styles.css" rel="stylesheet">
+    <title>Picross Solver</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="/src/styles.css" rel="stylesheet" />
   </head>
   <header>
-    
+    <h1>Picross Solver</h1>
+    <p>Picross, AKA nonogram, is a puzzle involving numbers.</p>
   </header>
-
   <main>
-   
+    <!-- grid size input, 2 numbers for x and y -->
+    <form></form>
+
+    <!-- actual puzzle -->
+    <form>
+      <!-- generated grid + input on top & left -->
+      <div></div>
+
+      <!-- solve button -->
+
+      <!-- clear grid button -->
+
+    </form>
   </main>
 </template>
 
